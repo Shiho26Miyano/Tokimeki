acb1322af724c01c7d02b002022cae0ac7814507
Yinan Hu <besthynlove@gmail.com>
2025-06-17 07:48:55 -0400

    test wsgi

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def handle_500(e):
 def handle_404(e):
     return jsonify({"error": "Resource not found"}), 404
 
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    debug = os.environ.get('FLASK_ENV') == 'development'
-    app.run(debug=debug, host='0.0.0.0', port=port) 
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port) 
